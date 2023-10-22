6c4b2aee8cde08fbdc1da0df501f123bba1f9573
James Nyamburi <nyamburijames@gmail.com>
2023-10-22 23:52:05 +0300

    lists commit

--- structuredData.py
+++ structuredData.py
@@ -4,3 +4,11 @@ numbers = [1,2,3]
 
 print(fruits[0])
 print(numbers)
+
+numbers[1] = 3
+print(numbers)
+
+fruits[2] = "mango"
+print(fruits)
+fruits[0:2] = "grapes","passion", "beetroot"
+print(fruits)
